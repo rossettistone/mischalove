84d99eba6e0f8144527366d8dc166b8e69030cd6
Mark Rossetti <mark.a.rossetti@gmail.com>
2016-11-07 12:18:55 -0800

    Changed DB connection alias

--- config/secrets.js
+++ config/secrets.js
@@ -1,7 +1,7 @@
 var settings = require('./settings');
 
 module.exports = {
-  db: process.env.MONGOLAB_URI || 'mongodb://localhost:27017/' + settings.machineTitle,
+  db: process.env.MONGODB_URI || 'mongodb://localhost:27017/' + settings.machineTitle,
   localAuth: true,
   sessionSecret: "Your Session Secret goes here",
 };
